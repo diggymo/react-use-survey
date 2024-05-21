ec7af554e254c6e97c5adc311a89797f7e31cc59
diggymo <morifuji.kodai@classmethod.jp>
2024-05-21 16:06:34 +0900

    test: `use`からthrowされるPromiseを無理やりcatchしてみた

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,26 @@ const fetchMyName: () => Promise<string> = () => {
 
   return cachedPromise;
 };
+
 const App = () => {
   console.log("RENDERING!");
+  const [count, setCount] = useState(0);
 
-  const myName = useCustom(fetchMyName());
+  let myName;
+  try {
+    myName = use(fetchMyName());
+  } catch (err) {
+    console.log({ err });
+  }
 
   console.log("DATA FETCHED!");
 
-  return <p>{myName}</p>;
+  return (
+    <div>
+      <p>{myName}</p>
+      <button onClick={() => setCount(count + 1)}>{count}</button>;
+    </div>
+  );
 };
 
 export const AppContainer = () => {
@@ -34,102 +46,4 @@ export const AppContainer = () => {
       </Suspense>
     </ErrorBoundary>
   );
-};
-
-let cache: Promise<unknown> | null = null;
-let resolvedValue:
-  | {
-      status: "fullfiled";
-      value: unknown;
-    }
-  | {
-      status: "rejected";
-      error: unknown;
-    }
-  | null = null;
-
-const useCustom = <T,>(promise: Promise<T>): T => {
-  const [, setKey] = useState(0);
-  const onForceUpdate = () => {
-    setKey((key) => key + 1);
-  };
-
-  const isCached = cache !== null;
-  console.log({ isResolved: resolvedValue !== null, isCached });
-  if (isCached) {
-    if (resolvedValue === null) throw cache;
-    if (resolvedValue.status === "fullfiled") {
-      return resolvedValue.value as T;
-    }
-    if (resolvedValue !== null && resolvedValue.status === "rejected") {
-      throw resolvedValue.error;
-    }
-
-    console.warn("不明なステータス", resolvedValue);
-  }
-
-  promise
-    .then((v) => {
-      resolvedValue = { status: "fullfiled", value: v };
-      onForceUpdate();
-    })
-    .catch((error) => {
-      resolvedValue = { status: "rejected", error };
-      onForceUpdate();
-    });
-
-  cache = promise;
-  throw promise;
-};
-
-const useCustom2 = <T,>(promise: Promise<T>): T => {
-  const [, setKey] = useState(0);
-  const [finalValue, setFinalValue] = useState<
-    | { status: "fullfiled"; value: T }
-    | { status: "rejected"; error: unknown }
-    | null
-  >(null);
-  const [savedPromise, setSavedPromise] = useState<Promise<T> | null>(null);
-
-  const onForceUpdate = () => {
-    setKey((key) => key + 1);
-  };
-
-  const isResolved = finalValue !== null;
-  const isCached = savedPromise !== null;
-  console.log({ finalValue, savedPromise });
-  if (isCached) {
-    if (!isResolved) throw savedPromise;
-    if (finalValue !== null && finalValue.status === "fullfiled") {
-      return finalValue.value as T;
-    }
-    if (finalValue !== null && finalValue.status === "rejected") {
-      throw finalValue.error;
-    }
-
-    console.warn("不明なステータス");
-  }
-
-  promise
-    .then((v) => {
-      setFinalValue({ status: "fullfiled", value: v });
-      onForceUpdate();
-    })
-    .catch((error) => {
-      setFinalValue({ status: "rejected", error });
-      onForceUpdate();
-    });
-
-  setSavedPromise(promise);
-  throw promise;
-};
-
-const registerUser = (email: string) => {
-  fetchUserData(email);
-};
-
-const fetchUserData = (email: string) => {
-  axios.get("/users", {
-    email,
-  });
 };
